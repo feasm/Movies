ff7afb9426441e8b522aebec8e4d4e8667d681e6
Felipe Melo <felipe.melo@btgpactual.com>
2020-08-26 21:03:37 -0300

    Project basic structure

---
+++ Movies/Movies/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Movies
+//
+//  Created by BTG Pactual on 26/08/20.
+//  Copyright © 2020 Felipe Melo. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+
+        return true
+    }
+
+
+}
+
---
+++ Movies/Movies/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Movies
+//
+//  Created by BTG Pactual on 26/08/20.
+//  Copyright © 2020 Felipe Melo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func loadView() {
+        super.loadView()
+        view = UIView(frame: UIScreen.main.bounds)
+        view.backgroundColor = .systemBlue
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
